f2852c0a482225d0006fd27c00e4f356c5b5821e
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-10-21 02:50:05 +0530

    added getStats route

--- backend/models/issues.js
+++ backend/models/issues.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const issueSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  category: { type: String, required: true },
+  category: { type: String },
   description: { type: String, required: true },
   location: {
     name: { type: String },
